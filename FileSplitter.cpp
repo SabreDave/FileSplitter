3f67f5491a84da6bd4c5367a1f281871f3710951
SabreDave <34918494+SabreDave@users.noreply.github.com>
2017-12-28 15:58:03 +0000

    Create FileSplitter.cpp

---
+++ FileSplitter.cpp
@@ -0,0 +1,15 @@
+// FileSplitter.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	if (argc < 2)
+		{
+		printf("Please Supply Source Filename"); 
+		}
+	return 0;
+}
+
